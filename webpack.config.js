2c33ee05d9061851c39c12afeead772a8afdf4c9
Cassandra <cassandra.moore1516@gmail.com>
2025-07-17 19:03:02 -0500

    change line 8 to const isProd = process.env.NODE_ENV !='development';

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const Dotenv = require('dotenv-webpack');
 const sass = require('sass'); // Explicitly using Dart Sass engine
 
 // ✅ Define environment mode flag code fixed form const isProd = process.env.NODE_ENV ==='production'; to line 8
-const isProd = process.env.NODE_ENV =='production';
+const isProd = process.env.NODE_ENV !='development';
 console.log("prod", isProd);
 console.log (process.env.NODE_ENV)
 module.exports = {
